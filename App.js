b8d0e4667328b7b7252f0092017313dda82f879e
asmaaelalfy <noureleslam065@gmail.com>
2021-08-08 08:56:34 +0200

    reactnative blog

---
+++ App.js
@@ -0,0 +1,25 @@
+import "react-native-gesture-handler";
+
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import MainTabs from "./navigation";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MainTabs />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
